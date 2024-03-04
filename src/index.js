270e431c0151073a9d522009ca6add560184212b
minskunn <157802869+minskunn@users.noreply.github.com>
2024-03-04 11:04:44 +0100

    Added search function using API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
+function refreshWeatherData(response) {
+  let temperatureNow = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#current-city");
+  temperatureNow.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "04e13864eb3t2f8af00833563db7ofc6";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeatherData);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#search-city");
-  let h1 = document.querySelector("#current-city");
-  h1.innerHTML = cityInput.value;
+  let cityElement = document.querySelector("#current-city");
+  searchCity(cityInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Oslo");
